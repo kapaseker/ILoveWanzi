0c36a94d6fd14364818ad481d7492f04697b3da2
xiepg <xiepg@meixing.com>
2018-07-12 16:20:08 +0800

    新的扩展作业

--- app/src/main/java/com/example/hengwang/mywanzi/TipActivity.java
+++ app/src/main/java/com/example/hengwang/mywanzi/TipActivity.java
@@ -1,6 +1,5 @@
 package com.example.hengwang.mywanzi;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -8,7 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.widget.Toast;
 
-public class TipActivity extends Activity {
+public class TipActivity extends AssignmentActivity {
     View mBtnToast;
     View mBtnDialog;
     View mBtnSnackbar;
