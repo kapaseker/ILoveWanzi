16ae7827df1d9d961758d64bf5a9f66f4a867d58
kapaseker <panoogunker@gmail.com>
2018-07-13 16:43:51 +0800

    提交作业

--- app/src/main/java/com/example/hengwang/mywanzi/GridViewActivity.java
+++ app/src/main/java/com/example/hengwang/mywanzi/GridViewActivity.java
@@ -2,6 +2,15 @@ package com.example.hengwang.mywanzi;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class GridViewActivity extends AssignmentActivity {
 
@@ -9,5 +18,44 @@ public class GridViewActivity extends AssignmentActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gridview);
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+
+        //生成动态数组，并且转入数据
+        ArrayList<HashMap<String, Object>> lstImageItem = new ArrayList<HashMap<String, Object>>();
+        for (int i = 0; i < 24; i++) {
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("ItemImage", R.drawable.icon_house);//添加图像资源的ID
+            map.put("ItemText", "NO." + String.valueOf(i));//按序号做ItemText
+            lstImageItem.add(map);
+        }
+        //生成适配器的ImageItem <====> 动态数组的元素，两者一一对应
+        SimpleAdapter saImageItems = new SimpleAdapter(this, //没什么解释
+                lstImageItem,//数据来源
+                R.layout.activity_griditme,//night_item的XML实现
+
+                //动态数组与ImageItem对应的子项
+                new String[]{"ItemImage", "ItemText"},
+
+                //ImageItem的XML文件里面的一个ImageView,两个TextView ID
+                new int[]{R.id.ItemImage, R.id.ItemText});
+        //添加并且显示
+        gridview.setAdapter(saImageItems);
+        //添加消息处理
+        gridview.setOnItemClickListener(new ItemClickListener());
+    }
+
+    //当AdapterView被单击(触摸屏或者键盘)，则返回的Item单击事件
+    class ItemClickListener implements OnItemClickListener {
+        public void onItemClick(AdapterView<?> arg0,//The AdapterView where the click happened
+                                View arg1,//The view within the AdapterView that was clicked
+                                int arg2,//The position of the view in the adapter
+                                long arg3//The row id of the item that was clicked
+        ) {
+            //在本例中arg2=arg3
+            HashMap<String, Object> item = (HashMap<String, Object>) arg0.getItemAtPosition(arg2);
+            //显示所选Item的ItemText
+            setTitle((String) item.get("ItemText"));
+            Toast.makeText(GridViewActivity.this, item.get("ItemText").toString(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
