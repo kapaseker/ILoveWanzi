ab497c5dcc7f5d51b29ca313287d4af82647d86f
kapaseker <panoogunker@gmail.com>
2018-07-24 15:10:59 +0800

    提交联系人作业

--- app/src/main/java/com/example/hengwang/mywanzi/OpenContractAssignmentActivity.java
+++ app/src/main/java/com/example/hengwang/mywanzi/OpenContractAssignmentActivity.java
@@ -1,9 +1,12 @@
 package com.example.hengwang.mywanzi;
 
+import android.content.Intent;
+
 public class OpenContractAssignmentActivity extends ArrangeAssignmentActivity {
 
     @Override
     protected void onCommitAssigment() {
+        startActivity(new Intent(this, contactActivity.class));
 
     }
 }
