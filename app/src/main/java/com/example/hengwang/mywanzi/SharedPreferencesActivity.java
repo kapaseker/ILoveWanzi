265f7562e94531ba77aca36309074cb6e7f1923c
kapaseker <panoogunker@gmail.com>
2018-07-27 16:31:23 +0800

    增加外部存储

--- app/src/main/java/com/example/hengwang/mywanzi/SharedPreferencesActivity.java
+++ app/src/main/java/com/example/hengwang/mywanzi/SharedPreferencesActivity.java
@@ -3,12 +3,15 @@ package com.example.hengwang.mywanzi;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -20,6 +23,7 @@ public class SharedPreferencesActivity extends AssignmentActivity implements Vie
     private static final String NAME_PREFERENCE = "love_qian";
     private static final String KEY_DATA = "data_value";
     private static final String NAME_SAVEFILE = "loveqian.txt";
+    private static final String NAME_SAVEEXTEERNALFILE = "loveqian.txt";
 
 
     TextView mEditView;
@@ -141,6 +145,86 @@ public class SharedPreferencesActivity extends AssignmentActivity implements Vie
         mEditView.setText(sb.toString());
     }
 
+
+    private void writeExternalfile() {
+        if (isSDCardMounted()) {
+            File dir = Environment.getExternalStorageDirectory();
+            File file = new File(dir, NAME_SAVEEXTEERNALFILE);
+            String str = mEditView.getText().toString();
+
+            FileOutputStream fos = null;
+            if (!str.isEmpty()) {
+                try {
+                    fos = openFileOutput(NAME_SAVEEXTEERNALFILE, Context.MODE_PRIVATE);
+                    fos.write(str.getBytes());
+                    fos.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(SharedPreferencesActivity.this, "输入成功", Toast.LENGTH_SHORT).show();
+
+            } else {
+                Toast.makeText(SharedPreferencesActivity.this, "请输入内容", Toast.LENGTH_SHORT).show();
+            }
+
+
+        } else {
+            Toast.makeText(this, "外部存储不可用", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    private void readExternalfile() {
+        if (isSDCardMounted()) {
+            File dir = Environment.getExternalStorageDirectory();
+
+            FileInputStream ios = null;
+            try {
+                ios = openFileInput(NAME_SAVEEXTEERNALFILE);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader(ios));
+            StringBuilder sb = new StringBuilder();
+            String content;
+
+            try {
+                content = br.readLine();
+                while (content != null) {
+                    sb.append(content);
+                    content = br.readLine();
+
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+
+                try {
+                    if (br != null) {
+                        br.close();
+                    }
+                    if (ios != null) {
+                        ios.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            mEditView.setText(sb.toString());
+
+
+        } else {
+            Toast.makeText(this, "外部存储不可用", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -156,6 +240,57 @@ public class SharedPreferencesActivity extends AssignmentActivity implements Vie
             case R.id.btn_readinter:
                 readFromInternalStorage();
                 break;
+            case R.id.btn_writeOuter:
+                writeExternalfile();
+                break;
+            case R.id.btn_readOuter:
+                readExternalfile();
+                break;
+
         }
     }
+
+
+    // 判断SD卡是否被挂载
+    public static boolean isSDCardMounted() {
+        // return Environment.getExternalStorageState().equals("mounted");
+        return Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED);
+    }
+
+    // 获取SD卡的根目录
+    public static String getSDCardBaseDir() {
+        if (isSDCardMounted()) {
+            return Environment.getExternalStorageDirectory().getAbsolutePath();
+        }
+        return null;
+    }
+
+    // 往SD卡的私有Files目录下保存文件
+    public static boolean saveFileToSDCardPrivateFilesDir(byte[] data,
+                                                          String type, String fileName, Context context) {
+        BufferedOutputStream bos = null;
+        if (isSDCardMounted()) {
+            File file = context.getExternalFilesDir(type);
+            try {
+                bos = new BufferedOutputStream(new FileOutputStream(new File(
+                        file, fileName)));
+                bos.write(data);
+                bos.flush();
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    bos.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+        return false;
+    }
+
+
 }
